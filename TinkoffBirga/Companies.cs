1b0ed557cb0f8274efcd5727310f35874d95acf7
BackMZ <ilia556.igorevich.t@gmail.com>
2024-02-01 20:53:50 +0300

    Созданы классы Companies и Deals

---
+++ TinkoffBirga/Companies.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinkoffBirga
+{
+    internal class Companies
+    {
+        int companyID;
+        static int IDCounter = 0;
+        string nameCompany;
+        string legalAddress;
+        string email;
+        string INN;
+
+        public int CompanyID { get => companyID; set => companyID = value; }
+        public string NameCompany { get => nameCompany; set => nameCompany = value; }
+        public string LegalAddress { get => legalAddress; set => legalAddress = value; }
+        public string Email { get => email; set => email = value; }
+        public string CompanyINN { get => INN; set => INN = value; }
+
+        public Companies(string nameCompany, string legalAddress, string email, string INN)
+        {
+            this.CompanyID = IDCounter;
+            this.NameCompany = nameCompany;
+            this.LegalAddress = legalAddress;
+            this.Email = email;
+            this.INN = INN;
+            IDCounter++;
+        }
+
+        public Companies()
+        {
+            this.CompanyID = -1;
+            this.NameCompany = null;
+            this.LegalAddress = null;
+            this.Email = null;
+            this.INN = null;
+        }
+    }
+}
---
+++ TinkoffBirga/Deals.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinkoffBirga
+{
+    internal class Deals
+    {
+        int dealID;
+        int FirstCompanyID;
+        int SecondCompanyID;
+
+        public int DealID { get => dealID; set => dealID = value; }
+        public int FirstCompanyID1 { get => FirstCompanyID; set => FirstCompanyID = value; }
+        public int SecondCompanyID1 { get => SecondCompanyID; set => SecondCompanyID = value; }
+
+        public Deals(int dealID, int firstCompanyID, int secondCompanyID)
+        {
+            this.DealID = dealID;
+            FirstCompanyID1 = firstCompanyID;
+            SecondCompanyID1 = secondCompanyID;
+        }
+
+        public Deals()
+        {
+            this.DealID = -1;
+            FirstCompanyID1 = -1;
+            SecondCompanyID1 = -1;
+        }
+    }
+}
